fc352e457849f4c2b906966f93b5b44cf557a8ee
unknown <prmalakoti@gmail.com>
2022-12-23 17:49:13 +0530

    commit #6

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express'),
-  appRoutes = express(),
-  bodyParser = require('body-parser'),
+const express = require('express');
+  let app1 = express();
+  app1.disable("x-powered-by");
+  let bodyParser = require('body-parser');
   port = process.env.API_APP_ENV || 8888;
+
+  let helmet = require("helmet");
+  let app = express(); // Compliant
+  app.use(helmet.hidePoweredBy());
 
 const apiRoute = require('./controllers/api'),
   middlewares = require('./middleware/common-middleware');
 
-appRoutes.use(bodyParser.json());
-appRoutes.use(middlewares.setDefaultHeaders);
+app.use(bodyParser.json());
+app.use(middlewares.setDefaultHeaders);
 
-appRoutes.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('This is the node api app !');
 });
 
 // set common response headers for all incoming requests
-appRoutes.use('/api', apiRoute);
+app.use('/api', apiRoute);
 
 if (!module.parent) {
-  appRoutes.listen(port, () => console.log('App booted'));
+  app.listen(port, () => console.log('App booted'));
 }
 
-module.exports = appRoutes;
+module.exports = app;
